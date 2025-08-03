51c6d372c4021d3a15802f83b5ff60077609cde8
osama <osamaabidoo99@gmail.com>
2025-08-03 21:25:32 +0200

    before file prossecing

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI , APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 import os
+from helpers.config import get_settings, Settings
 
 base_router = APIRouter(
-  prefix="/api/v1" , tags=["api_v1 "]
+    prefix="/api/v1",
+    tags=["api_v1"],
 )
-@base_router.get("/")
 
-async def welcome():
-  app_name = os.getenv("APP_NAME")
-  app_version = os.getenv("APP_VERSION")
-  return{
-    "app_name": app_name,
-    "app_version":app_version
-  }
+@base_router.get("/")
+async def welcome(app_settings: Settings = Depends(get_settings)):
+
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
+
+    return {
+        "app_name": app_name,
+        "app_version": app_version,
+    }
